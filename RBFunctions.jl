74557d259b160772e2e6c05308b1ded9c9b611cd
Volodymyr Yelnyk <elnikvova@gmail.com>
2023-03-22 12:06:08 +0100

    the code is written fully, automatic differentiation doesn't work

--- RBFunctions.jl
+++ RBFunctions.jl
@@ -27,7 +27,41 @@ function generate_2D_Halton_points(N) # 2-3 Halton sequence looks random enough
     return float(list)
 end
 
+function generate_2D_equally_spaced_points(N) # interiror + boundary points on unit square 
+    x = LinRange(0,1,N)
+    y = LinRange(0,1,N)
+    y = y * ones(N)'
+    x = ones(N) * x'
+    points = hcat(vec(x),vec(y))'
+    N_internal = (N-2)^2
+    N_boundary = N^2 - N_internal
+    boundary_points = zeros((2,N_boundary))
+    internal_points = zeros((2,N_internal))
+    idx_b, idx_i = 1,1
+    for i in 1:N^2
+        if 0.0 ∈ points[:,i] || 1.0 ∈ points[:,i]
+            boundary_points[:,idx_b] = points[:,i]
+            idx_b += 1
+        else
+            internal_points[:,idx_i] = points[:,i]
+            idx_i +=1
+        end
+    end
+    return internal_points,boundary_points
+end
 
+function point_difference_tensor(points1,points2) # reates NxMx2 tensor 
+    l1 = size(points1)[2]
+    l2 = size(points2)[2]
+    A = [ (points1[k,i] - points2[k,j]) for i=1:l1, j=1:l2, k=1:2]
+    return A
+end
+
+function apply(func, tensor)
+    l1,l2,l3 = size(tensor)
+    A = [ func(tensor[i,j,:]) for i=1:l1, j=1:l2]
+    return A
+end
 
 function linear(r,ϵ) # Linear RBF
     return abs(r)
